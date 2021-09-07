4e5c1c8bcea017dbfaedb22a362992ff9026c2e0
mutafow <bmutafov@gmail.com>
2021-09-07 16:32:38 +0300

    feat: add animation to the svg in blocking overlay

--- react-app/src/pages/BlockOverlay.tsx
+++ react-app/src/pages/BlockOverlay.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 const BlockOverlay: React.FC = () => {
   return (
     <div className="block-overlay">
-      <img src={process.env.PUBLIC_URL + '/block-mouse-icon.png'} alt="Block-mouse-icon" width="600" />
-      <h1>Please do not touch the mouse and keyboard during population</h1>
+      <img src={process.env.PUBLIC_URL + '/block_mouse_animation.svg'} alt="Block-mouse-icon" width="600" />
+      <div className="text-container">
+        <h1>Do not touch the mouse or keyboard during population!</h1>
+        <h2>Doing so may result in errors or wrong values</h2>
+      </div>
     </div>
   );
 };
