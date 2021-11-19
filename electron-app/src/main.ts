5b5c977e205e93b49c14a25dc6da84cc02af73f4
mutafow <bmutafov@gmail.com>
2021-11-19 12:09:23 +0200

    feat: make input speed 'extra-slow'

--- electron-app/src/main.ts
+++ electron-app/src/main.ts
@@ -99,7 +99,7 @@ class Main {
   };
 
   public getInputSpeed = (): InputSpeed => {
-    return 'slow';
+    return 'extra-slow';
     // return this.store.get(INPUT_SPEED_STORAGE_KEY) as InputSpeed;
   };
 
