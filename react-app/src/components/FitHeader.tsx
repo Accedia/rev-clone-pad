ea820ef211adfa53f52239f8cd8b35c9ff1316ac
mutafow <bmutafov@gmail.com>
2022-06-10 14:56:11 +0300

    fix: decrease input speed further

--- react-app/src/components/FitHeader.tsx
+++ react-app/src/components/FitHeader.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Message } from 'semantic-ui-react';
 import { electron } from '@react-app/utils/electron_remote';
 
-interface FitHeaderProps {}
+interface FitHeaderProps { }
 
 const FitHeader: React.FC<FitHeaderProps> = () => {
+  const getVersion = () => {
+    const { app } = electron.remote
+
+    if (app.isPackaged) {
+      return 'v' + electron.remote.app.getVersion();
+    } else {
+      return <span style={{ color: 'red', fontWeight: 'bold' }}>DEVELOPMENT</span>;
+    }
+  }
   return (
     <Message icon>
       <img className="fit-settings-logo" src={process.env.PUBLIC_URL + '/icon.ico'} alt="logo" />
       <Message.Content>
         <Message.Header>Force Import Technology</Message.Header>
         <div className="version-text">
-          <span className="version-label">Version:</span> v{electron.remote.app.getVersion()}
+          <span className="version-label">Version:</span> {getVersion()}
         </div>
       </Message.Content>
     </Message>
