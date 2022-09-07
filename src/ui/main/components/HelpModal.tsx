1c0bf9a3b1e9aeddb1c05b7178f5cf7cb406e92e
Angel Angelov <flushbg@gmail.com>
2022-09-07 14:20:49 +0300

    fix: Change asset path resolution

--- src/ui/main/components/HelpModal.tsx
+++ src/ui/main/components/HelpModal.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createStyles, Image, List, Modal, Text } from '@mantine/core';
+import checkbox_select from '../../../../assets/checkbox-select.png';
+import clone_button from '../../../../assets/clone-button.png';
 
 interface HelpModalProps {
   opened: boolean;
@@ -35,7 +37,7 @@ const HelpModal: React.FC<HelpModalProps> = ({ opened, onClose }) => {
         In order to populate the clipboard fields, you first need to clone a forgettable inside REV
         Clone Pad.
       </Text>
-      <List size="sm" spacing={6} mb='xs'>
+      <List size="sm" mb="xs" spacing={6}>
         <List.Item>
           In Portal, navigate to <b>Estimatics → Select Estimate</b>
         </List.Item>
@@ -46,12 +48,12 @@ const HelpModal: React.FC<HelpModalProps> = ({ opened, onClose }) => {
           Click on <b>Generals</b> or <b>Specifics</b>
         </List.Item>
         <List.Item>Select the forgettables that you want to clone</List.Item>
-        <Image className={classes.image} src="assets/checkbox-select.png" />
+        <Image className={classes.image} src={checkbox_select} />
         <List.Item>
           Navigate to the <b>Review / Commit</b> tab
         </List.Item>
         <List.Item>Click on the clone button</List.Item>
-        <Image className={classes.image} src="assets/clone-button.png" />
+        <Image className={classes.image} src={clone_button} />
         <List.Item>
           Forgettable data will be cloned into REV Clone Pad, so you can transfer it to CCC using
           the clipboard.
